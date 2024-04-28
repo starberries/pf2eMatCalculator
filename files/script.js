485c070ec4b4d03156e2a7390a77823bd8914407
Starberries <maistarberries@gmail.com>
2024-04-28 19:28:59 -0400

    Add calculation functions

--- files/script.js
+++ files/script.js
@@ -27,3 +27,31 @@ gradeList.forEach(option =>
       new Option(option.text, option.value)
     )
   );
+
+// Functions
+function getBulkPrice(m, g, i) {
+    let basePrice = data[m][g][i]
+    let bulkPrice = basePrice / 10
+    return bulkPrice
+  };
+  
+  function getMaterialAmount(c, g) {
+      let gradePercentage = 1
+    
+    if (g == "low") { gradePercentage = 0.1 }
+    else if (g == "standard") { gradePercentage = 0.25 }
+    
+    let materialAmount = (c * gradePercentage)
+    
+    return materialAmount
+  }
+  
+  function getTotalPrice(m, g, i) {
+    let bulk = document.querySelector('.bulk').value;
+    let basePrice = data[m][g][i];
+    
+    let bulkPrice = getBulkPrice(m, g, i);
+    
+    let totalPrice = (bulk * bulkPrice) + basePrice
+    return totalPrice
+  };
